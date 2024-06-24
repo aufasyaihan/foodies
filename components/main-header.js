f8b4ca1a593d976d7c82cbbdf1acec55184e2286
aufa azzahidi <aufa.azzahidi@gmail.com>
2024-06-24 11:37:32 +0700

    chore: Update global CSS and layout components

--- components/main-header.js
+++ components/main-header.js
@@ -3,27 +3,37 @@ import classes from "@/components/main-header.module.css";
 
 import logoImg from "@/assets/logo.png";
 import Image from "next/image";
+import MainHeaderBackground from "./main-header-background";
 
 export default function MainHeader() {
   return (
-    <header className={classes.header}>
-      <Link className={classes.logo} href="/">
-        <Image src={logoImg.src} alt="NextLevel Food" width={50} height={50} priority />
-        <h1>NextLevel Food</h1>
-      </Link>
-      <nav className={classes.nav}>
-        <ul>
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-          <li>
-            <Link href="/meals">Meals</Link>
-          </li>
-          <li>
-            <Link href="/meals/share">Share</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <>
+      <MainHeaderBackground />
+      <header className={classes.header}>
+        <Link className={classes.logo} href="/">
+          <Image
+            src={logoImg.src}
+            alt="NextLevel Food"
+            width={50}
+            height={50}
+            priority
+          />
+          <h1>NextLevel Food</h1>
+        </Link>
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/meals">Meals</Link>
+            </li>
+            <li>
+              <Link href="/meals/share">Share</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
   );
 }
