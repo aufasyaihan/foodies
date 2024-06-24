b4e2abc44ff96f9cc070ec6e99ad3c990457fb6b
aufa azzahidi <aufa.azzahidi@gmail.com>
2024-06-24 20:09:43 +0700

    refactor: Update DetailMealPage component with image, header, and instructions

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,9 +1,34 @@
-export default function DetailMeal({ params }) {
+import Image from "next/image";
+import classes from "./page.module.css";
+import { getMeal } from "@/lib/meals";
+
+export default function DetailMealPage({ params }) {
+  const meal = getMeal(params.slug);
+
+  const formattedInstructions = meal.instructions.replace(/\n/g, "<br>");
+
   return (
-    <div>
-      <h1>Detail Meal</h1>
-      <p>Welcome to the Detail Meal Page!</p>
-      <p>{params.slug}</p>
-    </div>
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: formattedInstructions,
+          }}
+        ></p>
+      </main>
+    </>
   );
 }
---
+++ app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <h1>Oops! This page doesn&apos;t exist.</h1>
+      <p>Try going back to the previous page or visit our homepage.</p>
+    </main>
+  );
+}
